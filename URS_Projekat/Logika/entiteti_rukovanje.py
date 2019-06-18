ec13dddd5e3809b0d2520fdbac36c4aa31dc53ef
Aleksandar Stojanovic <alestoj@hotmail.com>
2019-06-18 07:58:23 +0200

    Dodao klasu entiteti_rukovanje radi redefinisanja metoda

---
+++ URS_Projekat/Logika/entiteti_rukovanje.py
@@ -0,0 +1,12 @@
+class EntitetiRukovanje:
+    def __init__(self,putanja):
+        self.putanja = putanja
+
+    def citanje_svih(self):
+        pass
+    
+    def upis_jednog(self, entitet):
+        pass
+    
+    def izmjena_jednog(self, entitet):
+        pass
