f1c0984c34c4f8bd490eb0dbdeade6bf8b2c6a51
Aleksandar Stojanovic <alestoj@hotmail.com>
2019-06-18 09:37:16 +0200

    Dodata dokumentacija za sekcija_rukovanje

--- URS_Projekat/Logika/sekcija_rukovanje.py
+++ URS_Projekat/Logika/sekcija_rukovanje.py
@@ -2,11 +2,13 @@ from Entiteti.sekcija import Sekcija
 from Logika.entiteti_rukovanje import EntitetiRukovanje
 
 class Sekcija_rukovanje(EntitetiRukovanje):
+    """Klasa za rukovanje sekcijama"""
 
     def __init__(self, putanja):
         super().__init__(putanja)
 
     def citanje_svih(self):
+        """Citanje svih entiteta iz fajla"""
         with open(self.putanja, "r") as file:
             podaci = []
             for linija in file.readlines():
@@ -14,14 +16,17 @@ class Sekcija_rukovanje(EntitetiRukovanje):
             return podaci
 
     def napravi_entitet(self, linija):
+        "Metoda za pravljenje novog entiteta u formatu sa ; delimiterom iz __str__ funkcije"
         polje = linija.strip().split(";")
         return Sekcija(polje[0], polje[1], polje[2], polje[3])
 
     def upis_jednog(self, entitet):
+        """Metoda za upis novog entiteta"""
         with open(self.putanja, "a") as file:
             file.write(str(entitet))
-    
+
     def izmjena_jednog(self, entitet):
+        """Izmjeni entitet sa podacima novog entiteta"""
         entiteti = self.citanje_svih()
         pronasao = False
         for i in range(len(entiteti)):
@@ -39,6 +44,7 @@ class Sekcija_rukovanje(EntitetiRukovanje):
             return pronasao
         
     def pretraga_po_oznaci(self, lst, rijec):
+        """Pretraga entiteta po oznaci"""
         pretrazeno = []
         for i in lst:
             if i.oznaka.lower().find(rijec.lower()) != -1:
@@ -46,6 +52,7 @@ class Sekcija_rukovanje(EntitetiRukovanje):
         return pretrazeno
 
     def pretraga_po_nazivu(self, lst, rijec):
+        """Pretraga entiteta po nazivu"""
         pretrazeno = []
         for i in lst:
             if i.naziv.lower().find(rijec.lower()) != -1:
@@ -53,6 +60,7 @@ class Sekcija_rukovanje(EntitetiRukovanje):
         return pretrazeno
 
     def pretraga_po_opisu(self, lst, rijec):
+        """Pretraga entiteta po opisu"""
         pretrazeno = []    
         for i in lst:
             if i.opis.lower().find(rijec.lower()) != -1:
@@ -60,6 +68,7 @@ class Sekcija_rukovanje(EntitetiRukovanje):
         return pretrazeno
     
     def pretraga_po_poziciji(self, lst, rijec):
+        """Pretraga entiteta po poziciji"""
         pretrazeno = []
         for i in lst:
             if  i.pozicija.lower().find(rijec.lower()) != -1:
@@ -67,6 +76,7 @@ class Sekcija_rukovanje(EntitetiRukovanje):
         return pretrazeno
 
     def izlistaj_police(self, sekcija, lista_polica):
+        """Prikazivanje svih polica u odredjenoj sekciji"""
         police_sa_sekcijama = []
         for i in lista_polica:
             if i.sekcija == sekcija:
@@ -74,6 +84,7 @@ class Sekcija_rukovanje(EntitetiRukovanje):
         return police_sa_sekcijama
 
     def izlistaj_artikle(self, polica, lista_artikala):
+        """Prikazivanje svih artikala koji se nalaze na polici"""
         artikli_na_polici = []
         for i in lista_artikala:
             if i.polica == polica:
@@ -81,6 +92,7 @@ class Sekcija_rukovanje(EntitetiRukovanje):
         return artikli_na_polici
 
     def izlistaj_police_i_artikle(self, sekcija, lista_polica, lista_artikala):
+        """Prikazivanje svih polica u odredjenoj sekciji, i artikala koje se nalaze na tim policama"""
         police_i_artikli = []
         for i in lista_polica:
             if i.sekcija == sekcija:
